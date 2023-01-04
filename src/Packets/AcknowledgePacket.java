0fc6f1dda4c702aca033da9ae4eda758fe13ed09
Flashness123 <lukas.kouba.de@gmail.com>
2023-01-04 19:49:48 +0100

    ...

--- src/Packets/AcknowledgePacket.java
+++ src/Packets/AcknowledgePacket.java
@@ -1,4 +1,8 @@
 package Packets;
 
-public class AcknowledgePacket {
+public class AcknowledgePacket extends Packet {
+    private static final int AckNumber_Length = 1;
+    private static final int MaximalPackageNumber_Length = 1;   //1 da nur stop and wait, gbn wahrscheinlich nicht machen
+
+
 }
