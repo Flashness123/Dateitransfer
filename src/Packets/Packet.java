062c02a28880eaead69384836ba40004762e7db7
Flashness123 <lukas.kouba.de@gmail.com>
2023-01-04 18:57:25 +0100

    fehler gemacht und in stratpacket geschrieben, commit uberschreiben

--- src/Packets/Packet.java
+++ src/Packets/Packet.java
@@ -1,4 +1,17 @@
 package Packets;
 
+import java.nio.ByteBuffer;
+import java.nio.charset.StandardCharsets;
+
 public class Packet {
+    protected static final int SessionNumberLength = 2;//vererbt und fur erbende klassen sichtbar
+    protected static final int Crc32Length = 4;
+    protected static final int PaketNumberLength = 1;
+
+    protected ByteBuffer byteBuffer;
+
+    public byte[] toBytes(){
+        byte[] bytes = this.byteBuffer.array();
+        return bytes;
+    }
 }
--- src/Packets/StartPacket.java
+++ src/Packets/StartPacket.java
@@ -4,14 +4,5 @@ import java.nio.ByteBuffer;
 import java.nio.charset.StandardCharsets;
 
 public class StartPacket {
-    protected static final int SessionNumberLength = 2;//vererbt und fur erbende klassen sichtbar
-    protected static final int Crc32Length = 4;
-    protected static final int PaketNumberLength = 1;
 
-    protected ByteBuffer byteBuffer;
-
-    public byte[] toBytes(){
-        byte[] bytes = this.byteBuffer.array();
-        return bytes;
-    }
 }
